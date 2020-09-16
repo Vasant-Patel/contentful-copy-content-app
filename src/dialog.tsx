9e26bf541ea9113c9570dcd042ba99ea87dc5e31
Vasant Patel <vasant.patel@polarspin.com>
2020-09-16 15:41:08 +0300

    Added basic auth inputs for copyEntry function

--- src/dialog.tsx
+++ src/dialog.tsx
@@ -2,7 +2,15 @@ import React, { useEffect, useState, ChangeEvent } from 'react';
 import { DialogExtensionSDK } from 'contentful-ui-extensions-sdk';
 import * as R from 'ramda';
 import axios from 'axios';
-import { Button, Option, SelectField } from '@contentful/forma-36-react-components';
+import {
+  Button,
+  HelpText,
+  Option,
+  SelectField,
+  Table,
+  TableRow,
+  TextField,
+} from '@contentful/forma-36-react-components';
 
 interface State {
   url: string;
@@ -13,7 +21,9 @@ export default function Dialog({ sdk }: { sdk: DialogExtensionSDK }) {
   const [entryId, setEntryId] = useState<string | undefined>();
   const [environments, setEnvironments] = useState<string[]>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>();
+  const [error, setError] = useState<string | undefined>();
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassowrd] = useState<string>('');
 
   // backend api's ---------
   const loadEnvironments = () => {
@@ -37,17 +47,26 @@ export default function Dialog({ sdk }: { sdk: DialogExtensionSDK }) {
       })
       .then((r) => r.data as string[])
       .then((r) => setEnvironments(r))
-      .catch(e => setError(e.messsage || e))
+      .catch((e) => setError(e.messsage || e))
       .finally(() => {
         setLoading(false);
       });
   };
 
-
   console.log('SDK in dialog => ', sdk);
-  const onSelectionChange = (e: ChangeEvent) => {
+  const onSelectionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log('Env selected =>', e.target.value);
     setTargetEnv(e.target.value);
+  };
+
+  const onUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedValue = e.target.value;
+    setUsername(updatedValue);
+  };
+
+  const onPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedValue = e.target.value;
+    setPassowrd(updatedValue);
   };
 
   useEffect(() => {
@@ -63,7 +82,7 @@ export default function Dialog({ sdk }: { sdk: DialogExtensionSDK }) {
   }, []);
 
   useEffect(() => {
-    console.log("Error : ", error)
+    console.log('Error : ', error);
     if (error) {
       sdk.notifier.error(error);
     }
@@ -71,7 +90,7 @@ export default function Dialog({ sdk }: { sdk: DialogExtensionSDK }) {
 
   useEffect(() => {
     if (environments.length) {
-      setTargetEnv(R.head(environments))
+      setTargetEnv(R.head(environments));
     }
   }, [environments]);
 
@@ -88,19 +107,25 @@ export default function Dialog({ sdk }: { sdk: DialogExtensionSDK }) {
 
     setLoading(true);
     axios
-    .get(endpointUrl, {
-      params: {
-        entryId: entryId,
-        sourceEnv: sdk.ids.environment,
-        targetEnv,
-      },
-    })
-    .then((r) => r.data as string[])
-    .then(_ => sdk.notifier.success("Entry copied/updated successfully!"))
-    .catch(e => setError(e.messsage || e))
-    .finally(() => {
-      setLoading(false);
-    });
+      .get(endpointUrl, {
+        params: {
+          entryId: entryId,
+          sourceEnv: sdk.ids.environment,
+          targetEnv,
+        },
+        auth: {
+          username: username || '',
+          password: password || '',
+        },
+      })
+      .then((r) => r.data as string[])
+      .then((_) => sdk.notifier.success('Entry copied/updated successfully!'))
+      .catch((e) => {
+        setError(e.response?.message || e.message || e);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
@@ -110,7 +135,7 @@ export default function Dialog({ sdk }: { sdk: DialogExtensionSDK }) {
         name="optionSelect"
         id="optionSelect"
         labelText="Select copy destination environment or ALL for copying to all environments"
-        value={targetEnv || ''}
+        value={targetEnv}
         onChange={onSelectionChange}
         helpText="This function allows you to copy this entry to different release(s) i.e environment(s). Its useful when you have to update some texts in more than one environment.">
         {environments.map((e) => (
@@ -119,15 +144,38 @@ export default function Dialog({ sdk }: { sdk: DialogExtensionSDK }) {
           </Option>
         ))}
       </SelectField>
-      <div style={{ margin: 50 }} />
+
+      <div style={{ margin: 30 }} />
+
+      <div className="flexbox-container">
+        <TextField
+          name="user"
+          id="user"
+          value={username}
+          onChange={onUsernameChange}
+          labelText="Username"
+        />
+        <div style={{ width: 20 }} />
+        <TextField
+          name="pwd"
+          id="pwd"
+          value={password}
+          onChange={onPasswordChange}
+          labelText="Password"
+        />
+      </div>
+      <div style={{ margin: 10 }} />
+      <HelpText>Don't have crendentials? Contact VRMobile Team.</HelpText>
+      <div style={{ margin: 30 }} />
       <Button
         buttonType="negative"
         isFullWidth={true}
-        disabled={isLoading || !targetEnv}
+        disabled={isLoading || !targetEnv || !username || !password}
         onClick={onCopyPress}
         loading={isLoading}>
         {targetEnv ? `Copy entry to ${targetEnv} env` : 'Copy entry'}
       </Button>
+      <div style={{ margin: 50 }} />
     </div>
   );
 }
